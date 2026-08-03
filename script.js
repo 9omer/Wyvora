8f53d14062ec7154a6e1bc06e3c75929ea500ec6
9omer <omergulsoy143@gmail.com>
2026-08-03 11:16:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -365,7 +365,7 @@ function deleteProduct(index){
 
 
     let businessCode =
-JSON.parse(localStorage.getItem("businesses"))[0].code;
+localStorage.getItem("activeBusinessCode");
 
 let products =
 JSON.parse(localStorage.getItem("products_" + businessCode)) || [];
